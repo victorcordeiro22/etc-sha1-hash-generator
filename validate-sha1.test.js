6a73a7a267a7f292d519fc16048e7cc6393b6bef
Victor Cordeiro <vh2c@icloud.com>
2024-05-02 15:08:36 -0300

    updated validate-sha1 test

--- validate-sha1.test.js
+++ validate-sha1.test.js
@@ -8,7 +8,7 @@ describe("validateSHA1Signature", () => {
   };
 
   const validSignature = "a58060f85f92b4f35fbc0923c0fcaf7ae86f46f0";
-  const invalidSignature = "7d97bde750ce5404e97d5f042f95f89f1c235555";
+  const invalidSignature = "invalidSignature";
 
   it("should return true for a valid signature", () => {
     const result = validateSHA1Signature(validSignature, SECRET_TOKEN, payload);
@@ -17,6 +17,8 @@ describe("validateSHA1Signature", () => {
   });
 
   it("should return false for an invalid signature", () => {
+    jest.spyOn(console, "error").mockImplementation(() => {});
+    
     const result = validateSHA1Signature(
       invalidSignature,
       SECRET_TOKEN,
@@ -24,6 +26,8 @@ describe("validateSHA1Signature", () => {
     );
 
     expect(result).toBe(false);
+
+    console.error.mockRestore();
   });
 
   it("should return false if no signature", () => {
